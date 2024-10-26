61411d8209aa03eed6f1f33ba9e7504d0214fd3b
mohamed-ennahdi <mohamed.ennahdi@gmail.com>
2024-10-26 20:48:21 +0000

    Adding module objectmorph-app. (#4)

---
+++ objectmorph-app/src/main/java/com/github/ennahdi/objectmorph/objectmorph/ObjectmorphApplication.java
@@ -0,0 +1,13 @@
+package com.github.ennahdi.objectmorph.objectmorph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectmorphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectmorphApplication.class, args);
+	}
+
+}
