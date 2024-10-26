c15503d14e94c500ad2e5e63c0e99df7d035897b
mohamed-ennahdi <mohamed.ennahdi@gmail.com>
2024-10-26 21:13:26 +0000

    Adding module objectmorph-renderer. (#5)

---
+++ objectmorph-renderer/src/test/java/net/sourceforge/ennahdi/objectmorph/renderer/HTMLIndividualGeneratorTest.java
@@ -0,0 +1,16 @@
+package net.sourceforge.ennahdi.objectmorph.renderer;
+
+import java.net.URISyntaxException;
+
+import org.junit.jupiter.api.Test;
+
+public class HTMLIndividualGeneratorTest {
+
+	private HTMLIndividualGenerator createTestSubject() throws URISyntaxException {
+		return null;
+	}
+
+	@Test
+	public void generateFullClassHTMLTest() throws Exception {
+	}
+}
