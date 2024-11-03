3e98642356ad9310cee56cc5f871492abfa0ecea
mohamed-ennahdi <mohamed.ennahdi@gmail.com>
2024-11-03 13:49:48 +0000

    Discrepancy resolution + POM version property (#10)

---
+++ objectmorph-app/src/main/java/com/github/mohamedennahdi/objectmorph/app/logic/ObjectmorphComponent.java
@@ -0,0 +1,20 @@
+package com.github.mohamedennahdi.objectmorph.app.logic;
+
+import java.io.File;
+
+import org.springframework.stereotype.Component;
+
+import com.github.mohamedennahdi.objectmorph.renderer.HTMLGenerator;
+
+@Component
+public class ObjectmorphComponent {
+
+	HTMLGenerator htmlGenerator;
+	
+	private ObjectmorphComponent() {}
+
+	public  HTMLGenerator getHtmlGenerator(File... files) {
+		htmlGenerator = new HTMLGenerator(files);
+		return htmlGenerator;
+	}
+}
