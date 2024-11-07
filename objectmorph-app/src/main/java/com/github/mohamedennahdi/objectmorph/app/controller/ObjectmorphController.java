17afa348944bd07761cbacf4afb82e088003f132
mohamed-ennahdi <mohamed.ennahdi@gmail.com>
2024-11-07 20:36:10 +0000

    Endpoints re-organization + files overwriting (#14)

--- objectmorph-app/src/main/java/com/github/mohamedennahdi/objectmorph/app/controller/ObjectmorphController.java
+++ objectmorph-app/src/main/java/com/github/mohamedennahdi/objectmorph/app/controller/ObjectmorphController.java
@@ -1,20 +1,18 @@
 package com.github.mohamedennahdi.objectmorph.app.controller;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.mohamedennahdi.objectmorph.app.service.ObjectmorphService;
 
 import io.swagger.v3.oas.annotations.Parameter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 public class ObjectmorphController {
 	
@@ -25,16 +23,6 @@ public class ObjectmorphController {
 	}
 	
 	@CrossOrigin(origins = "*")
-	@RequestMapping( value = "/hello", method =  RequestMethod.GET )
-	public ResponseEntity<String> hello() {
-		return new ResponseEntity<>( "Hello World", HttpStatus.OK);
-	}
-	
-	@RequestMapping( value = "/test", method =  RequestMethod.GET )
-	public ResponseEntity<String> test(List<String> files) {
-		return new ResponseEntity<>( "Hello World", HttpStatus.OK);
-	}
-	
 	@GetMapping( value = "/html" )
 	public ResponseEntity<String> html(@Parameter(name = "sourceCode", description = "Source Code", example = "class SourceCode { int attribute1; int attribute2; SourceCode(){} public int getAttribute1() { return attribute1;}}") 
 									   @RequestParam("sourceCode")	String[] sourceCode) {
@@ -43,8 +31,7 @@ public class ObjectmorphController {
 			html = objectmorphService.generateHTML(sourceCode);
 			return new ResponseEntity<>(html, HttpStatus.OK);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.error("Error in ObjectmorphController", e);
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
