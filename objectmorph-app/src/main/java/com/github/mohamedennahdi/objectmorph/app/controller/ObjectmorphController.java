28fea3587810170306910fd8ee0574b706d4ed56
Mohamed ENNAHDI EL IDRISSI <mohamed.ennahdi@gmail.com>
2025-01-03 17:16:06 +0000

    Client now sends filename with its respective source code. (#32)

--- objectmorph-app/src/main/java/com/github/mohamedennahdi/objectmorph/app/controller/ObjectmorphController.java
+++ objectmorph-app/src/main/java/com/github/mohamedennahdi/objectmorph/app/controller/ObjectmorphController.java
@@ -6,9 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.mohamedennahdi.objectmorph.app.dto.SourceCodeDto;
 import com.github.mohamedennahdi.objectmorph.app.service.ObjectmorphService;
 
 import io.swagger.v3.oas.annotations.Parameter;
@@ -30,7 +31,7 @@ public class ObjectmorphController {
 	@CrossOrigin(origins = "*")
 	@PostMapping( value = "/html" )
 	public ResponseEntity<String> html(@Parameter(name = "sourceCode", description = "Source Code", example = "class SourceCode { int attribute1; int attribute2; SourceCode(){} public int getAttribute1() { return attribute1;}}") 
-									   @RequestParam("sourceCode")	String[] sourceCode) {
+									   @RequestBody	SourceCodeDto[] sourceCode) {
 		try {
 			String html = "";
 			html = objectmorphService.generateHTML(sourceCode);
