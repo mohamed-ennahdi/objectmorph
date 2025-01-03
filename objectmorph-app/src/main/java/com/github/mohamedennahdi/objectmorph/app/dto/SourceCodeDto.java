2b1b5481a2eb01470af01ee9af3b5fc23e4e2525
Mohamed ENNAHDI EL IDRISSI <mohamed.ennahdi@gmail.com>
2025-01-03 17:14:16 +0000

    Client now sends filename with its respective source code.

---
+++ objectmorph-app/src/main/java/com/github/mohamedennahdi/objectmorph/app/dto/SourceCodeDto.java
@@ -0,0 +1,9 @@
+package com.github.mohamedennahdi.objectmorph.app.dto;
+
+import lombok.Data;
+
+@Data
+public class SourceCodeDto {
+	private String filename;
+	private String sourceCode;
+}
--- objectmorph-app/src/main/java/com/github/mohamedennahdi/objectmorph/app/service/ObjectmorphService.java
+++ objectmorph-app/src/main/java/com/github/mohamedennahdi/objectmorph/app/service/ObjectmorphService.java
@@ -8,11 +8,10 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 
+import com.github.mohamedennahdi.objectmorph.app.dto.SourceCodeDto;
 import com.github.mohamedennahdi.objectmorph.app.logic.ObjectmorphComponent;
 
 import lombok.extern.slf4j.Slf4j;
@@ -26,29 +25,22 @@ public class ObjectmorphService {
 		this.objectmorphLogic = objectmorphLogic;
 	}
 
-	public String generateHTML(String[] sourceCodes) throws Exception {
+	public String generateHTML(SourceCodeDto[] sourceCodes) throws Exception {
 		List<File> files = new ArrayList<>(); 
 		try {
-			String patternString = "(?<=class ).*?(?=\\s)";
-	        Pattern pattern = Pattern.compile(patternString);
-	        
 	        String path = System.getProperty("user.home");
-			for (String  sourceCode: sourceCodes) {
-				sourceCode = URLDecoder.decode(sourceCode, Charset.forName("UTF-8"));
-				Matcher matcher = pattern.matcher(sourceCode);
-				if (matcher.find()) {
-					String fileName = matcher.group() + ".java";
-					log.info("Filename: " + fileName);
-					File file = new File(path + File.separator + fileName);
-					try (Writer fileWriter = new FileWriter(file, false)) {
-						fileWriter.write(sourceCode);
-					} catch (Exception e) {
-						throw e;
-					}
-					files.add(file);
+			for (SourceCodeDto sourceCode: sourceCodes) {
+				sourceCode.setSourceCode(URLDecoder.decode(sourceCode.getSourceCode(), Charset.forName("UTF-8")));
+				String fileName = sourceCode.getFilename();
+				log.info("Filename: " + fileName);
+				File file = new File(path + File.separator + fileName);
+				try (Writer fileWriter = new FileWriter(file, false)) {
+					fileWriter.write(sourceCode.getSourceCode());
+				} catch (Exception e) {
+					throw e;
 				}
+				files.add(file);
 			}
-			
 			return objectmorphLogic.getHtmlGenerator(files.toArray(new File[0])).generateFullHTML();
 		} catch (Exception e) {
 			throw e;
